9586ed8baef6599ad692438fadcfa860c3a321bd
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2020-08-18 23:10:38 -0500

    i add better changes on project

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -4,23 +4,19 @@
  * and open the template in the editor.
  */
 document.write("<h1>Programa para contar el número de digitos de un número entero positivo</h1>");
+document.write('<p><label>Escribe tu número entero para evaluar la cantidad de números que contiene: </label> <input id="numtest" type="text" name="numtest" /> <button onclick="showQuantity()">Evaluar</button></p>')
+document.write('<div id="result"></div>');
 
 function showQuantity(){
-   var numTest = parseInt(prompt("Escribe tu número entero para evaluar la cantidad de números que contiene:"));
+    console.log("self.value", document.getElementById("numtest").value);
+   var numTest = parseInt(document.getElementById("numtest").value);
     if (numTest > 0) {
         if(String(numTest).length === 1){
-            document.write('El número proporcionado tiene '+String(numTest).length+' digito <br>');
+            document.getElementById("result").innerHTML += 'El número '+numTest+' tiene '+String(numTest).length+' digito <br>';
         }else{
-            document.write('El número proporcionado tiene '+String(numTest).length+' digitos <br>');
+            document.getElementById("result").innerHTML += 'El número '+numTest+' tiene '+String(numTest).length+' digitos <br>';
         }
     } else {
         alert("El número entero que ingreses debe ser positivo");
-        this.reload();
     }
 }
-function reload(){
-    location.reload();
-}
-this.showQuantity();
-
-document.write("<br><button onclick='reload()'>Evaluar cantidad de números que contiene el número entero</button>");
